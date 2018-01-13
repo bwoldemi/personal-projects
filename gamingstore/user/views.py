5d18a94e02a2e624a039367dc31563fe3de766b3
woldemb1 <woldemb1@aalto.fi>
2018-01-13 19:19:40 +0200

    updating template file

--- gamingstore/user/views.py
+++ gamingstore/user/views.py
@@ -1,6 +1,10 @@
-# Create your views here.
+#from django.shortcuts import render
+
 from django.http import HttpResponse
 from django.shortcuts import render
 
+
+
+# Create your views here.
 def index(request):
-    return render(request, 'user/index.html')
+        return render(request,'user/index.html')
