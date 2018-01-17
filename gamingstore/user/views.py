84c6abe73cee8295715271ce1d33780de311da03
woldemicael <bereketka@gmail.com>
2018-01-17 13:31:41 +0200

    updating user int

--- gamingstore/user/views.py
+++ gamingstore/user/views.py
@@ -3,10 +3,16 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
-
+from django import forms
 
 # Create your views here.
 def index(request):
     return render(request,'user/index.html')
 def register(request):
-    return render(request, 'user/register.html')
+    if request.method=='GET':
+        print("** a get request ")
+        return render(request, 'user/register.html')
+    elif request.method=='POST':
+        class ExampleForms(forms.Form):
+            firstname=forms.CharField()
+        return render(request,'user/register.html')
